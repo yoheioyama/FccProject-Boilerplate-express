b0d7b69b3a0b40c160a6f2bb21f1a3218bbaa21c
Yohei Oyama <yoheioyama@Yoheis-MacBook-Air.local>
2021-11-25 20:04:18 -0800

    Add a key message and value hello json

--- myApp.js
+++ myApp.js
@@ -9,6 +9,11 @@ app.get("/", (req, res) => {
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", (req, res) => {
+    res.json({
+      message: "Hello json"
+    });
+  });
 
 
 
