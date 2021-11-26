89603f3aa9aa64d1eec59153e425964d9b4375ca
Yohei Oyama <yoheioyama@Yoheis-MacBook-Air.local>
2021-11-25 19:29:04 -0800

    Mount the express.static() to myApp.js

--- myApp.js
+++ myApp.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 
 console.log('Hello World');
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
 
-
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
